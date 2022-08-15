63dbe56a141a8de9df9d304d714a0d989de9b8d7
smitkiller <wsxmit@gmail.com>
2022-08-15 09:11:13 +0700

    test

--- myprice/views.py
+++ myprice/views.py
@@ -19,9 +19,9 @@ def index(request):
 		img_file = settings.IMAGE_ROOT + '/' + str(request.FILES['upload_image'])
 		csv_file = settings.MEDIA_ROOT + '/' + 'index.csv'
 		if form.is_valid():
-			# return HttpResponseRedirect('/?submitted=True')	
 			form.save()
-			submitted = True
+			return HttpResponseRedirect('/?submitted=True')			
+			# submitted = True
 
 	else:
 		form = UploadForm
