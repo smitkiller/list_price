038c58b515c6ad0026c040988c65f9c1eef736dd
smitkiller <wsxmit@gmail.com>
2022-08-24 11:30:51 +0700

    add admin css js

--- list_price/settings.py
+++ list_price/settings.py
@@ -115,13 +115,14 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 IMAGE_ROOT = os.path.join(MEDIA_ROOT, 'images')
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
