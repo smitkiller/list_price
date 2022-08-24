ec1bf722fb64607c2fa4cef4d9a0a8e418cce934
smitkiller <wsxmit@gmail.com>
2022-08-24 10:11:29 +0700

    add admin css js

--- list_price/settings.py
+++ list_price/settings.py
@@ -118,7 +118,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 IMAGE_ROOT = os.path.join(MEDIA_ROOT, 'images')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (
+    BASE_DIR / "static",
+    # "http://secure-refuge-67481.herokuapp.com/static/"
+    )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
